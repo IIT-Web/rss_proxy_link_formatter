62dbdefc1736f95d45c0fc0a4b03d121ceafe657
Brian Bailey <bbailey4@iit.edu>
2017-02-06 14:04:48 -0600

    added parameter to the admin form to allow user to set custom url parameter

--- ajax-rss-proxy-link.tpl.php
+++ ajax-rss-proxy-link.tpl.php
@@ -3,8 +3,7 @@
 /**
  * @file ajax-rss-proxy-link.tpl.php
  * Default template implementation to display the value of a link field with
- * Ajax Rss Feed formatter.
- *
+ * Ajax Proxy of RSS Link formatter.
  *
  * Available variables:
  * - $variables: Array of variables passed to the template
@@ -40,32 +39,32 @@
 ?>
 
 <div id="rss_id_<?php print $variables['id']; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-	<div class="rss-feed-title"><?php print $element['title']; ?></div>
-	<div class="rss-feed-contents"></div>
-	<div class="rss-feed-button"></div>
-	<script type="text/javascript">
-		jQuery(document).ready(function($){
-			var proxy_url = "<?php print $variables['proxy_url']; ?>";
-			var jqxhr = $.ajax(proxy_url).done(function(data){
-				//console.log(data);
-				if (data.channel) {
-					var html = '<ul>';
-					for (var i = 0; i < data.channel.item.length; i++) {
-						var title = data.channel.item[i].title;
-						var link = data.channel.item[i].link;;
-						html += '<li><a target="_blank" href="' + link + '" title="' + title + '">' + title + '</a></li>';
-					};
-					html += '</ul>';
-					$("#rss_id_<?php print $variables['id']; ?> .rss-feed-contents").append(html);
-					var button = '<a target="_blank" href="' + data.channel.link + '" title="<?php print $element['title']; ?>">Go to <?php print $element['title']; ?></a>';
-					$("#rss_id_<?php print $variables['id']; ?> .rss-feed-button").append(button);
-				} else {
-					var RSS_link = '<?php print $element['url']; ?>';
-					$("#rss_id_<?php print $variables['id']; ?> .rss-feed-contents").append('<a href="' + RSS_link + '">' + RSS_link + '</a>');
-				}
-			}).fail(function(jqXHR, textStatus, err){
-				console.log('Feed Load Error Status: ' + textStatus + ': ' + err);
-			});
-		});
-	</script>
+  <div class="rss-feed-title"><?php print $element['title']; ?></div>
+  <div class="rss-feed-contents"></div>
+  <div class="rss-feed-button"></div>
+  <script type="text/javascript">
+    jQuery(document).ready(function($){
+      var proxy_url = "<?php print $variables['proxy_url']; ?>";
+      var jqxhr = $.ajax(proxy_url).done(function(data){
+        //console.log(data);
+        if (data.channel) {
+          var html = '<ul>';
+          for (var i = 0; i < data.channel.item.length; i++) {
+            var title = data.channel.item[i].title;
+            var link = data.channel.item[i].link;;
+            html += '<li><a target="_blank" href="' + link + '" title="' + title + '">' + title + '</a></li>';
+          };
+          html += '</ul>';
+          $("#rss_id_<?php print $variables['id']; ?> .rss-feed-contents").append(html);
+          var button = '<a target="_blank" href="' + data.channel.link + '" title="<?php print $element['title']; ?>">Go to <?php print $element['title']; ?></a>';
+          $("#rss_id_<?php print $variables['id']; ?> .rss-feed-button").append(button);
+        } else {
+          var RSS_link = '<?php print $element['url']; ?>';
+          $("#rss_id_<?php print $variables['id']; ?> .rss-feed-contents").append('<a href="' + RSS_link + '">' + RSS_link + '</a>');
+        }
+      }).fail(function(jqXHR, textStatus, err){
+        console.log('Feed Load Error Status: ' + textStatus + ': ' + err);
+      });
+    });
+  </script>
 </div>
